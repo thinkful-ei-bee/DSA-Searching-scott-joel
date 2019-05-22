6b94184c1af45c827b78829894cad5551b6b188f
Joel Corey <joelcorey@fastmail.com>
2019-05-22 11:30:11 -0700

    echo out found yes / no

--- search-demo/src/component/Search.js
+++ search-demo/src/component/Search.js
@@ -7,7 +7,7 @@ export default class Search extends Component {
       list: '89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5',
       steps: null,
       searchFor: null,
-      found: null
+      found: false
     }
   }
 
@@ -31,7 +31,7 @@ export default class Search extends Component {
       }
     }
     this.setState({ steps: count, found: false });
-    return -1;
+    return;
   }
 
   handleBinarySearch = () => {
@@ -44,7 +44,7 @@ export default class Search extends Component {
     if (start > end) return -1;
     //find the midpoint and the item at the midpoint
     let index = Math.floor((start + end) / 2);
-    console.log(`start: ${array[start]}, end: ${array[end]}, midpoint: ${array[index]}`)
+    //console.log(`start: ${array[start]}, end: ${array[end]}, midpoint: ${array[index]}`)
     let item = array[index];
     //if the middle element is the target them return that location
     if (item === value) {
@@ -67,7 +67,7 @@ export default class Search extends Component {
   }
 
   render() {
-
+    //let this.state.found)
     return (
       <div>
         <p>{this.state.list}</p>
@@ -78,6 +78,13 @@ export default class Search extends Component {
         <br />
 
         <p>Steps: {this.state.steps}</p>
+        <p>
+          Found: 
+          {this.state.found
+            ? ' Yes'
+            : ' No'
+          }
+        </p>
       </div>
     );
   }
